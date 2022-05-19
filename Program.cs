f3a77cc4e58a14285408558a454f7f1d53b690d1
KonstantinosMr <100868977+KonstantinosMr@users.noreply.github.com>
2022-05-19 19:14:39 +0300

    added a filtered stream with 1 filter:java

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Tweetinvi;
 using Tweetinvi.Models;
 
@@ -6,13 +7,25 @@ namespace TwitterApp190522
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
 
             var userCredentials = new TwitterCredentials("nN2yYQcMWlhsvaa8awvHo9PWF", "MDGDCAAhOaA3BSVMSz5HF2LElow39rdJlPJxdQO9z1E4pZ03b3", "1629692186-BimLBhF2MUQZ5ufDZ4G93ufBy7byABzijbZcNqQ", "hdY2py8hMvdSb7tU0a0wDxLmkKZrH7Gj24P7XL0SpAjTo");
             var userClient = new TwitterClient(userCredentials);
+
+            // Create a simple stream containing only tweets with the keyword France
+
+            var stream = userClient.Streams.CreateFilteredStream();
+            stream.AddTrack("java");
+
+            stream.MatchingTweetReceived += (sender, eventReceived) =>
+            {
+                Console.WriteLine(eventReceived.Tweet);
+            };
+
+            await stream.StartMatchingAnyConditionAsync();
         }
     }
 }
