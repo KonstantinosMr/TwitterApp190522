cc7133d3be24978be5dafbe9c380745f67f3cca6
KonstantinosMr <100868977+KonstantinosMr@users.noreply.github.com>
2022-05-19 19:06:40 +0300

    added tweetinvi library. addedd credentials.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Tweetinvi;
+using Tweetinvi.Models;
 
 namespace TwitterApp190522
 {
@@ -7,6 +9,10 @@ namespace TwitterApp190522
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+            var userCredentials = new TwitterCredentials("nN2yYQcMWlhsvaa8awvHo9PWF", "MDGDCAAhOaA3BSVMSz5HF2LElow39rdJlPJxdQO9z1E4pZ03b3", "1629692186-BimLBhF2MUQZ5ufDZ4G93ufBy7byABzijbZcNqQ", "hdY2py8hMvdSb7tU0a0wDxLmkKZrH7Gj24P7XL0SpAjTo");
+            var userClient = new TwitterClient(userCredentials);
         }
     }
 }
